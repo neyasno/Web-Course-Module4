74eaab76d74d289ef81cf945c996841f18dd5ab6
neyasno <65032545+neyasno@users.noreply.github.com>
2024-11-14 15:38:49 +0500

    last

---
+++ mod4_solution/script.js
@@ -0,0 +1,21 @@
+
+this.helloSpeaker = helloSpeaker;
+this.byeSpeaker = byeSpeaker;
+(
+  function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+
+}
+)();
